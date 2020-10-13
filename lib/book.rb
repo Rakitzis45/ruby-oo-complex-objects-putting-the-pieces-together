67e5955b97d74c9fee1e337578ff1eb6881bf52a
Rakitzis45 <michael.rakitzis@gmail.com>
2020-10-13 17:39:34 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    def new_book
+        Book.new(title)
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
